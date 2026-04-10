f2c706b4148c8f810a968a932311e1a901e782c6
sairam9216 <51757885+sairam9216@users.noreply.github.com>
2026-04-10 14:37:06 +0000

    Update launch.py

--- projects/hello-world/hello-world-python/launch.py
+++ projects/hello-world/hello-world-python/launch.py
@@ -2,6 +2,6 @@ from flask import Flask
 helloworld = Flask(__name__)
 @helloworld.route("/")
 def run():
-    return "{\"message\":\"Hello swarana v1\"}"
+    return "{\"message\":\"Hello ram v2\"}"
 if __name__ == "__main__":
     helloworld.run(host="0.0.0.0", port=int("4000"), debug=True)
